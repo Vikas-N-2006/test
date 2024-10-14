b404ee6ed9eb17f6002f24cd010bb419959bcb8f
Vikas-N <vikassharadha2006@gmail.com>
2024-10-14 14:36:53 +0530

    Updated and renamed hello.c to hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+void add(){
+  cout<<"Addition"<<endl;
+}
+void sub(){
+  cout<<"Subtraction"<<endl;
+}
+  void mul(){
+  cout<<"Multiplication"<<endl;
+}
+cout<<"My name is Vikas.N"<<endl;
+  cout<<"This is DSCE"<<endl;
+ cout<<"Vgit lab"<<endl;
+}
